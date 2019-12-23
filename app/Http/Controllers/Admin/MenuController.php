c38e1ce4ed514b6e092ef8e73f6079b0d17e12f8
Danilo <brickk1337@gmail.com>
2019-12-23 21:20:16 +0200

    menu has been changed

--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -4,21 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Menu;
 
 class MenuController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    public function update(Request $request, $id)
-    {
-        $menu = Menu::find($id);
-        $update_data = $request->only($menu->getFillable());
-        $menu->update($update_data);
-
-        return back()->with('status', 'Successful updated!');
-    }
+    //
 }
